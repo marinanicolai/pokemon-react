1e86fa10a5f61cbc5e59b04be7f50ed09890ec57
marina <nicolaimarina@gmail.com>
2023-05-01 14:51:10 -0400

    adding the example page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Poke from "./Poke";
+import Example from "./Example";
 
 const App = () => {
   return (
     <div className="App">
       <div className="poke">
         <Poke />
+      </div>
+      <div id="example" className="example">
+        <Example />
       </div>
     </div>
   );
